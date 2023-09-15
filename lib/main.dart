3c9968dc4160f8f2dc8bce311b5a63012d0bb3f7
C4r50nC <xichen059@gmail.com>
2023-09-15 15:31:03 +1000

    refactor: extract Quiz class from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:adv_basics/start_screet.dart';
+import 'package:adv_basics/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepPurple,
-                Colors.purpleAccent,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
