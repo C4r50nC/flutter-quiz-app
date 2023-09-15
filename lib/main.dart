cfb2853f065524cb994b3787be5630dd5767d167
C4r50nC <xichen059@gmail.com>
2023-09-15 14:36:04 +1000

    refactor: implement StartScreen class to optimize code structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/start_screet.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            color: Colors.deepPurple,
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 300,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 40),
-                ),
-                const Text(
-                  'Learn Flutter the fun way!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 40),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.deepPurple),
-                  ),
-                  child: const Text('Start Quiz!'),
-                )
-              ],
-            ),
-          ),
-        ),
+        body: StartScreen(),
       ),
     ),
   );
---
+++ lib/start_screet.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.deepPurple,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+              ),
+              child: const Text('Start Quiz!'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
