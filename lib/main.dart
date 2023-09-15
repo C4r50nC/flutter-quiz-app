6445d2e7166953069ffa04f2067b4be84b3c7eb5
C4r50nC <xichen059@gmail.com>
2023-09-15 14:20:52 +1000

    feat: implement main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.deepPurple,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 40),
+                ),
+                const Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 40),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.deepPurple),
+                  ),
+                  child: const Text('Start Quiz!'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
